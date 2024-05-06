8ca750ca326d1fa6ff784783cfa01c3f94e92a9d
shasocastris <fleetadmiralcaine@gmail.com>
2024-05-05 22:01:48 -0600

    Update bug catching contest pokemon

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -1,13 +1,14 @@
 ContestMons:
 	;      %, species,   min, max
-	dbwbb 20, CATERPIE,    7, 18
-	dbwbb 20, WEEDLE,      7, 18
-	dbwbb 10, METAPOD,     9, 18
-	dbwbb 10, KAKUNA,      9, 18
-	dbwbb  5, BUTTERFREE, 12, 15
-	dbwbb  5, BEEDRILL,   12, 15
-	dbwbb 10, VENONAT,    10, 16
-	dbwbb 10, PARAS,      10, 17
-	dbwbb  5, SCYTHER,    13, 14
-	dbwbb  5, PINSIR,     13, 14
+	dbwbb 15, LEDIAN,     18, 23
+	dbwbb 15, ARIADOS,    18, 23
+	dbwbb 10, VENOMOTH,   20, 27
+	dbwbb 10, BUTTERFREE, 17, 20
+	dbwbb 10, BEEDRILL,   17, 20
+	dbwbb 10, SCYTHER,    15, 21
+	dbwbb 10, PINSIR,     15, 22
+	dbwbb  5, PARASECT,   20, 27
+	dbwbb  5, FORRETRESS, 20, 27
+	dbwbb  5, SCIZOR,     18, 27
+	dbwbb  5, HERACROSS,  18, 27
 	dbwbb -1, VENOMOTH,   30, 40
