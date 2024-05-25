696e00cc47cb9fb8b5588a07e9619e7217edea32
shasocastris <fleetadmiralcaine@gmail.com>
2024-05-25 07:49:37 -0600

    Update bug contest pokemon list, levels: 18-34

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -1,14 +1,15 @@
 ContestMons:
 	;      %, species,   min, max
-	dbwbb 15, LEDIAN,     18, 23
-	dbwbb 15, ARIADOS,    18, 23
-	dbwbb 10, VENOMOTH,   20, 27
-	dbwbb 10, BUTTERFREE, 17, 20
-	dbwbb 10, BEEDRILL,   17, 20
-	dbwbb 10, SCYTHER,    15, 21
-	dbwbb 10, PINSIR,     15, 22
-	dbwbb  5, PARASECT,   20, 27
-	dbwbb  5, FORRETRESS, 20, 27
-	dbwbb  5, SCIZOR,     18, 27
-	dbwbb  5, HERACROSS,  18, 27
+	dbwbb 14, BUTTERFREE, 18, 28
+	dbwbb 14, BEEDRILL,   20, 30
+	dbwbb 14, LEDIAN,     18, 27
+	dbwbb 14, ARIADOS,    20, 28
+	dbwbb  9, VENOMOTH,   31, 34
+	dbwbb  9, FORRETRESS, 31, 34
+	dbwbb  9, PARASECT,   24, 29
+	dbwbb  5, SCYTHER,    21, 30
+	dbwbb  5, PINSIR,     22, 31
+	dbwbb  3, YANMA,      19, 28
+	dbwbb  3, SHUCKLE,    23, 28
+	dbwbb  1, HERACROSS,  23, 31
 	dbwbb -1, VENOMOTH,   30, 40
