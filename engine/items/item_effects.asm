ae89577a87c7876eee27a3613919061f9b79d4ca
shasocastris <fleetadmiralcaine@gmail.com>
2024-05-25 20:00:59 -0600

    GAin exp after catching Pokemon

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -538,6 +538,19 @@ PokeBallEffect:
 	call ClearSprites
 
 	ld a, [wTempSpecies]
+	ld l, a
+	ld a, [wCurPartyLevel]
+	ld h, a
+	push hl
+	farcall ApplyExperienceAfterEnemyCaught
+	pop hl
+	ld a, l
+	ld [wCurPartySpecies], a
+	ld [wTempSpecies], a
+	ld a, h
+	ld [wCurPartyLevel], a
+
+	ld a, [wTempSpecies]
 	call CheckCaughtMon
 
 	ld a, c
