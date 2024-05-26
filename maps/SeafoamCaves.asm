62e849ef4d5134e651d83ce1d7e998fb225a6ddc
shasocastris <fleetadmiralcaine@gmail.com>
2024-05-26 13:23:11 -0600

    Implement SeafoamCaves for Articuno

---
+++ maps/SeafoamCaves.asm
@@ -0,0 +1,18 @@
+	object_const_def
+
+SeafoamCaves_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+SeafoamCaves_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event 10, 16, ROUTE_20, 2
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
