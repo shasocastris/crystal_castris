09c48e69fd6bf2126f504ac9b39cfe013637fd3c
shasocastris <fleetadmiralcaine@gmail.com>
2024-08-28 14:36:12 -0600

    Add pp restoration items to bargain mart

--- data/items/bargain_shop.asm
+++ data/items/bargain_shop.asm
@@ -1,8 +1,12 @@
 BargainShopData:
-	db 5
+	db 9
 	dw NUGGET,     4500
 	dw PEARL,       650
 	dw BIG_PEARL,  3500
 	dw STARDUST,    900
 	dw STAR_PIECE, 4600
+	dw ETHER,      1100
+	dw MAX_ETHER,  1850
+	dw ELIXER,     2800
+	dw MAX_ELIXER, 4200
 	dw -1
