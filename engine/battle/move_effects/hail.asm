3a1b9437921ff0bde6ed41bfe6dcecb79fd4611c
shasocastris <fleetadmiralcaine@gmail.com>
2025-02-20 15:28:53 -0700

    Implement HELD_PROLONG_RAIN, SUN, SAND, HAIL item effects

--- engine/battle/move_effects/hail.asm
+++ engine/battle/move_effects/hail.asm
@@ -7,7 +7,23 @@ BattleCommand_StartHail:
 
 	ld a, WEATHER_HAIL
 	ld [wBattleWeather], a
+
+	push bc
+	push de
+	push hl
+	call GetUserItem
+	ld a, b
+	cp HELD_PROLONG_HAIL
+	pop hl
+	pop de
+	pop bc
+
+	jr z, .prolong_hail
 	ld a, 5
+	jr .got_hail
+.prolong_hail
+	ld a, 8
+.got_hail
 	ld [wWeatherCount], a
 	call AnimateCurrentMove
 	ld hl, ItStartedToHailText
--- engine/battle/move_effects/sandstorm.asm
+++ engine/battle/move_effects/sandstorm.asm
@@ -1,11 +1,29 @@
 BattleCommand_StartSandstorm:
+; startsandstorm
+
 	ld a, [wBattleWeather]
 	cp WEATHER_SANDSTORM
 	jr z, .failed
 
 	ld a, WEATHER_SANDSTORM
 	ld [wBattleWeather], a
+
+	push bc
+	push de
+	push hl
+	call GetUserItem
+	ld a, b
+	cp HELD_PROLONG_SAND
+	pop hl
+	pop de
+	pop bc
+
+	jr z, .prolong_sand
 	ld a, 5
+	jr .got_sand
+.prolong_sand
+	ld a, 8
+.got_sand
 	ld [wWeatherCount], a
 	call AnimateCurrentMove
 	ld hl, SandstormBrewedText
