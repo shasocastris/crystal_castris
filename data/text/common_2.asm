65c7c44184025121dd5a6887647507a44a36c4aa
shasocastris <fleetadmiralcaine@gmail.com>
2025-03-26 19:09:37 -0600

    Add some random text for the Pokemon in the day care

--- data/text/common_2.asm
+++ data/text/common_2.asm
@@ -753,6 +753,21 @@ _BreedShowsInterestText::
 	text "."
 	prompt
 
+_VeryHappyText::
+	text "It looks very"
+	line "happy!"
+	prompt
+
+_PlayingAroundText::
+	text "It's playing"
+	line "around."
+	prompt
+
+_GrowingStrongText::
+	text "It's growing"
+	line "strong!"
+	prompt
+
 _EmptyMailboxText::
 	text "There's no MAIL"
 	line "here."
