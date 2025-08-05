fefd139c3d587b4883f3db1e807bfd886f1f99fa
shasocastris <fleetadmiralcaine@gmail.com>
2025-08-05 09:36:36 -0600

    Revert "Implement "re-use repel" tutorial"
    This reverts commit d936dc13b458101a90efc0468a11422c8954c41e.

--- ram/wram.asm
+++ ram/wram.asm
@@ -2874,7 +2874,6 @@ wPhoneList:: ds CONTACT_LIST_SIZE + 1
 wLuckyNumberShowFlag:: db
 wLuckyIDNumber:: dw
 
-wRepelType:: db
 wRepelEffect:: db ; If a Repel is in use, it contains the nr of steps it's still active
 wBikeStep:: dw
 wKurtApricornQuantity:: db
