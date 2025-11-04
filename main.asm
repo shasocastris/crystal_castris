f82ebc168cbaca6dbc670de806b8eafb7b62a4ec
shasocastris <fleetadmiralcaine@gmail.com>
2025-11-04 14:15:30 -0700

    Revert "Remove includes; WIP"
    This reverts commit 5bdb8609cca4ea6c7914db5e808e692b1d7568ba.

--- main.asm
+++ main.asm
@@ -1,5 +1,6 @@
 SECTION "bank1", ROMX
 
+INCLUDE "engine/link/place_waiting_text.asm"
 INCLUDE "engine/gfx/load_push_oam.asm"
 INCLUDE "engine/overworld/map_objects.asm"
 INCLUDE "engine/menus/intro_menu.asm"
@@ -197,6 +198,7 @@ INCLUDE "engine/phone/phonering_copytilemapatonce.asm"
 
 SECTION "bank13_2", ROMX
 
+INCLUDE "engine/link/link_2.asm"
 INCLUDE "engine/rtc/reset_password.asm"
 INCLUDE "engine/menus/delete_save.asm"
 INCLUDE "data/tilesets.asm"
@@ -228,6 +230,7 @@ INCLUDE "engine/pokemon/tempmon.asm"
 INCLUDE "engine/pokemon/types.asm"
 INCLUDE "engine/pokemon/categories.asm"
 INCLUDE "engine/pokemon/mon_stats.asm"
+INCLUDE "engine/link/init_list.asm"
 INCLUDE "engine/pokemon/experience.asm"
 INCLUDE "engine/pokemon/switchpartymons.asm"
 INCLUDE "engine/gfx/load_pics.asm"
@@ -450,9 +453,11 @@ INCLUDE "engine/movie/intro.asm"
 SECTION "bank3E", ROMX
 
 INCLUDE "engine/gfx/load_font.asm"
+INCLUDE "engine/link/time_capsule.asm"
 INCLUDE "engine/events/name_rater.asm"
 INCLUDE "engine/events/play_slow_cry.asm"
 INCLUDE "engine/pokedex/new_pokedex_entry.asm"
+INCLUDE "engine/link/time_capsule_2.asm"
 INCLUDE "engine/pokedex/unown_dex.asm"
 INCLUDE "engine/events/magikarp.asm"
 INCLUDE "engine/battle/hidden_power.asm"
@@ -503,6 +508,10 @@ INCLUDE "engine/events/battle_tower/trainer_text.asm"
 
 
 SECTION "bank5B", ROMX
+
+INCLUDE "engine/link/link_trade.asm"
+INCLUDE "engine/link/link.asm"
+INCLUDE "engine/battle/link_result.asm"
 
 
 SECTION "mobile5C", ROMX
