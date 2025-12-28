879c489ea31de6597538e8504bd43eae5f4cc45c
shasocastris <fleetadmiralcaine@gmail.com>
2025-12-28 10:36:41 -0700

    Carve off unnecessary trees on west of route 36

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -237,7 +237,7 @@ ENDM
 	newgroup VIOLET                                               ; 10
 	map_const ROUTE_32,                                    10, 45 ;  1
 	map_const ROUTE_35,                                    10, 18 ;  2
-	map_const ROUTE_36,                                    30,  9 ;  3
+	map_const ROUTE_36,                                    24,  9 ;  3
 	map_const ROUTE_37,                                    10,  9 ;  4
 	map_const VIOLET_CITY,                                 20, 18 ;  5
 	map_const VIOLET_MART,                                  6,  4 ;  6
--- data/maps/attributes.asm
+++ data/maps/attributes.asm
@@ -185,17 +185,17 @@ ENDM
 	connection east, AzaleaTown, AZALEA_TOWN, 18
 
 	map_attributes Route35, ROUTE_35, $05, NORTH | SOUTH
-	connection north, Route36, ROUTE_36, 0
+	connection north, Route36, ROUTE_36, 6
 	connection south, GoldenrodCity, GOLDENROD_CITY, -5
 
 	map_attributes Route36, ROUTE_36, $05, NORTH | SOUTH | EAST
-	connection north, Route37, ROUTE_37, 10
-	connection south, Route35, ROUTE_35, 0
+	connection north, Route37, ROUTE_37, 4
+	connection south, Route35, ROUTE_35, -6
 	connection east, VioletCity, VIOLET_CITY, 0
 
 	map_attributes Route37, ROUTE_37, $05, NORTH | SOUTH
 	connection north, EcruteakCity, ECRUTEAK_CITY, -5
-	connection south, Route36, ROUTE_36, -10
+	connection south, Route36, ROUTE_36, -4
 
 	map_attributes Route38, ROUTE_38, $05, WEST | EAST
 	connection west, Route39, ROUTE_39, 0
