6535fa5ef3cb8db3a6ec8509a0cb00066296f63f
shasocastris <fleetadmiralcaine@gmail.com>
2026-01-10 20:20:19 -0700

    Make new block for unactivated shrine

--- data/tilesets/johto_collision.asm
+++ data/tilesets/johto_collision.asm
@@ -154,3 +154,7 @@
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 99
 	tilecoll WALL, DOOR, FLOOR, FLOOR ; 9a
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 9b
+	tilecoll WALL, WALL, WALL, WALL ; 9c
+	tilecoll WALL, WALL, WALL, WALL ; 9d
+	tilecoll WALL, WALL, WALL, WALL ; 9e
+	tilecoll WALL, WALL, WALL, WALL ; 9f
