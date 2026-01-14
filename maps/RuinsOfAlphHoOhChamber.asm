779d637a423bd5995ab04986506da9af5214ad4f
shasocastris <fleetadmiralcaine@gmail.com>
2026-01-14 16:08:56 -0700

    Routed Ho-Oh chamber

--- maps/RuinsOfAlphHoOhChamber.asm
+++ maps/RuinsOfAlphHoOhChamber.asm
@@ -1,50 +1,17 @@
 RuinsOfAlphHoOhChamber_MapScripts:
 	def_scene_scripts
-	scene_script RuinsOfAlphHoOhChamberCheckWallScene, SCENE_RUINSOFALPHHOOHCHAMBER_CHECK_WALL
-	scene_script RuinsOfAlphHoOhChamberNoopScene,      SCENE_RUINSOFALPHHOOHCHAMBER_NOOP
 
 	def_callbacks
-	callback MAPCALLBACK_TILES, RuinsOfAlphHoOhChamberHiddenDoorsCallback
+	callback MAPCALLBACK_TILES, RuinsOfAlphHoOhChamberOpenHoleCallback
 
-RuinsOfAlphHoOhChamberCheckWallScene:
-	special HoOhChamber
-	checkevent EVENT_WALL_OPENED_IN_HO_OH_CHAMBER
-	iftrue .OpenWall
-	end
-
-.OpenWall:
-	sdefer RuinsOfAlphHoOhChamberWallOpenScript
-	end
-
-RuinsOfAlphHoOhChamberNoopScene:
-	end
-
-RuinsOfAlphHoOhChamberHiddenDoorsCallback:
-	checkevent EVENT_WALL_OPENED_IN_HO_OH_CHAMBER
-	iftrue .WallOpen
-	changeblock 4, 0, $2e ; closed wall
-.WallOpen:
+RuinsOfAlphHoOhChamberOpenHoleCallback:
 	checkevent EVENT_SOLVED_HO_OH_PUZZLE
 	iffalse .FloorClosed
 	endcallback
-
 .FloorClosed:
 	changeblock 2, 2, $01 ; left floor
 	changeblock 4, 2, $02 ; right floor
 	endcallback
-
-RuinsOfAlphHoOhChamberWallOpenScript:
-	pause 30
-	earthquake 30
-	showemote EMOTE_SHOCK, PLAYER, 20
-	pause 30
-	playsound SFX_STRENGTH
-	changeblock 4, 0, $30 ; open wall
-	refreshmap
-	earthquake 50
-	setscene SCENE_RUINSOFALPHHOOHCHAMBER_NOOP
-	closetext
-	end
 
 RuinsOfAlphHoOhChamberPuzzle:
 	reanchormap
@@ -79,49 +46,9 @@ RuinsOfAlphHoOhChamberAncientReplica:
 RuinsOfAlphHoOhChamberDescriptionSign:
 	jumptext RuinsOfAlphHoOhChamberDescriptionText
 
-RuinsOfAlphHoOhChamberWallPatternLeft:
-	opentext
-	writetext RuinsOfAlphHoOhChamberWallPatternLeftText
-	setval UNOWNWORDS_HO_OH
-	special DisplayUnownWords
-	closetext
-	end
-
-RuinsOfAlphHoOhChamberWallPatternRight:
-	checkevent EVENT_WALL_OPENED_IN_HO_OH_CHAMBER
-	iftrue .WallOpen
-	opentext
-	writetext RuinsOfAlphHoOhChamberWallPatternRightText
-	setval UNOWNWORDS_HO_OH
-	special DisplayUnownWords
-	closetext
-	end
-
-.WallOpen:
-	opentext
-	writetext RuinsOfAlphHoOhChamberWallHoleText
-	waitbutton
-	closetext
-	end
-
 RuinsOfAlphHoOhChamberSkyfallTopMovement:
 	skyfall_top
 	step_end
-
-RuinsOfAlphHoOhChamberWallPatternLeftText:
-	text "Patterns appeared"
-	line "on the walls…"
-	done
-
-RuinsOfAlphHoOhChamberWallPatternRightText:
-	text "Patterns appeared"
-	line "on the walls…"
-	done
-
-RuinsOfAlphHoOhChamberWallHoleText:
-	text "There's a big hole"
-	line "in the wall!"
-	done
 
 RuinsOfAlphHoOhChamberAncientReplicaText:
 	text "It's a replica of"
@@ -143,9 +70,8 @@ RuinsOfAlphHoOhChamber_MapEvents:
 	def_warp_events
 	warp_event  3,  9, RUINS_OF_ALPH_OUTSIDE, 1
 	warp_event  4,  9, RUINS_OF_ALPH_OUTSIDE, 1
-	warp_event  3,  3, RUINS_OF_ALPH_INNER_CHAMBER, 2
-	warp_event  4,  3, RUINS_OF_ALPH_INNER_CHAMBER, 3
-	warp_event  4,  0, RUINS_OF_ALPH_HO_OH_ITEM_ROOM, 1
+	warp_event  3,  3, RUINS_OF_ALPH_HO_OH_ITEM_ROOM, 1
+	warp_event  4,  3, RUINS_OF_ALPH_HO_OH_ITEM_ROOM, 2
 
 	def_coord_events
 
@@ -154,7 +80,5 @@ RuinsOfAlphHoOhChamber_MapEvents:
 	bg_event  5,  3, BGEVENT_READ, RuinsOfAlphHoOhChamberAncientReplica
 	bg_event  3,  2, BGEVENT_UP, RuinsOfAlphHoOhChamberPuzzle
 	bg_event  4,  2, BGEVENT_UP, RuinsOfAlphHoOhChamberDescriptionSign
-	bg_event  3,  0, BGEVENT_UP, RuinsOfAlphHoOhChamberWallPatternLeft
-	bg_event  4,  0, BGEVENT_UP, RuinsOfAlphHoOhChamberWallPatternRight
 
 	def_object_events
