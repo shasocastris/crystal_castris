9b5f537985ee2fba296e6c1b9b4078e9a0f0ff39
shasocastris <fleetadmiralcaine@gmail.com>
2026-01-15 16:37:18 -0700

    Add rockets/scientists to Ho-Oh chamber

--- maps/RuinsOfAlphHoOhChamber.asm
+++ maps/RuinsOfAlphHoOhChamber.asm
@@ -1,3 +1,8 @@
+	object_const_def
+	const RUINSOFALPHOUTSIDE_ROCKET_M23
+	const RUINSOFALPHOUTSIDE_ROCKET_F10
+	const RUINSOFALPHOUTSIDE_SCIENTIST_KIP
+
 RuinsOfAlphHoOhChamber_MapScripts:
 	def_scene_scripts
 
@@ -40,6 +45,36 @@ RuinsOfAlphHoOhChamberPuzzle:
 	warpcheck
 	end
 
+TrainerGruntM23:
+    trainer GRUNTM, GRUNTM_23, EVENT_BEAT_ROCKET_GRUNTM_23, GruntM23SeenText, GruntM23BeatenText, 0, .Script
+
+.Script:
+    opentext
+    writetext GruntM23AfterBattleText
+    waitbutton
+    closetext
+    end
+
+TrainerGruntF10:
+    trainer GRUNTF, GRUNTF_10, EVENT_BEAT_ROCKET_GRUNTF_10, GruntF10SeenText, GruntF10BeatenText, 0, .Script
+
+.Script:
+    opentext
+    writetext GruntF10AfterBattleText
+    waitbutton
+    closetext
+    end
+
+TrainerScientistKip:
+    trainer SCIENTIST, KIP, EVENT_BEAT_SCIENTIST_KIP, ScientistKipSeenText, ScientistKipBeatenText, 0, .Script
+
+.Script:
+    opentext
+    writetext ScientistKipAfterBattleText
+    waitbutton
+    closetext
+    end
+
 RuinsOfAlphHoOhChamberAncientReplica:
 	jumptext RuinsOfAlphHoOhChamberAncientReplicaText
 
@@ -49,6 +84,89 @@ RuinsOfAlphHoOhChamberDescriptionSign:
 RuinsOfAlphHoOhChamberSkyfallTopMovement:
 	skyfall_top
 	step_end
+
+GruntM23SeenText:
+    text "You got past the"
+    line "others?"
+
+    para "Impressive! But"
+    line "you stop here!"
+    done
+
+GruntM23BeatenText:
+    text "Guess not…"
+    done
+
+GruntM23AfterBattleText:
+    text "The BOSS said if"
+    line "we find these"
+    cont "#MON first…"
+
+    para "We'll control"
+    line "something the"
+    cont "LEAGUE can't touch!"
+
+    para "That's gotta be"
+    line "worth something!"
+    done
+
+GruntF10SeenText:
+    text "Stop right there!"
+
+    para "No one interrupts"
+    line "our work!"
+    done
+
+GruntF10BeatenText:
+    text "Tch! You're"
+    line "tougher than you"
+    cont "look!"
+    done
+
+GruntF10AfterBattleText:
+    text "The scientists are"
+    line "certain something"
+    cont "lives in here."
+
+    para "They keep talking"
+    line "about psychic"
+    cont "energy readings…"
+
+    para "Whatever. I'll"
+    line "believe it when I"
+    cont "see it!"
+    done
+
+ScientistKipSeenText:
+    text "Marvelous! You've"
+    line "made it this far!"
+
+    para "But I can't allow"
+    line "further intrusion!"
+    done
+
+ScientistKipBeatenText:
+    text "What remarkable"
+    line "tenacity!"
+    done
+
+ScientistKipAfterBattleText:
+    text "My readings show"
+    line "psychic energy"
+    cont "permeating the"
+    cont "ruins."
+
+    para "It intensifies"
+    line "near the puzzle"
+    cont "chambers."
+
+    para "If #MON are"
+    line "generating this"
+    cont "energy…"
+
+    para "They're close."
+    line "Very close indeed!"
+    done
 
 RuinsOfAlphHoOhChamberAncientReplicaText:
 	text "It's a replica of"
@@ -82,3 +200,6 @@ RuinsOfAlphHoOhChamber_MapEvents:
 	bg_event  4,  2, BGEVENT_UP, RuinsOfAlphHoOhChamberDescriptionSign
 
 	def_object_events
+	object_event  1,  7, SPRITE_ROCKET, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 4, TrainerGruntM23, -1
+	object_event  6,  7, SPRITE_ROCKET_GIRL, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 4, TrainerGruntF10, -1
+	object_event  3,  5, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 2, TrainerScientistKip, -1
