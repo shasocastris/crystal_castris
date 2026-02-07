f1ba9ea12c579efec92ed92e3e6599f9736517e1
shasocastris <fleetadmiralcaine@gmail.com>
2026-02-06 18:29:01 -0700

    Update rooftop sales

--- data/items/rooftop_sale.asm
+++ data/items/rooftop_sale.asm
@@ -1,17 +1,20 @@
 RooftopSaleMart1:
-	db 5
-	dw POKE_BALL,     150
-	dw GREAT_BALL,    500
-	dw SUPER_POTION,  500
+	db 6
+	dw ULTRA_BALL,   1000
+	dw HYPER_POTION, 1000
 	dw FULL_HEAL,     500
+	dw FULL_RESTORE, 2000
 	dw REVIVE,       1200
+	dw MAX_REPEL,     550
 	dw -1
 
 RooftopSaleMart2:
-	db 5
-	dw HYPER_POTION, 1000
-	dw FULL_RESTORE, 2000
-	dw FULL_HEAL,     500
-	dw ULTRA_BALL,   1000
-	dw PROTEIN,      7800
+	db 7
+	dw HP_UP,   5500
+	dw PROTEIN, 5500
+	dw IRON,    5500
+	dw CARBOS,  5500
+	dw CALCIUM, 5500
+	dw ZINC,    5500
+	dw PP_UP,   5500
 	dw -1
