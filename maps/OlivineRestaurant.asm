080a9edb0f522289f8c9505038de348200dfc642
shasocastris <fleetadmiralcaine@gmail.com>
2026-02-07 20:51:20 -0700

    Populate Olivine Restaurant

--- maps/OlivineRestaurant.asm
+++ maps/OlivineRestaurant.asm
@@ -9,8 +9,16 @@ OlivineRestaurant_MapScripts:
 OlivineRestaurantLassScript:
 	jumptextfaceplayer OlivineRestaurantLassText
 
+OlivineRestaurantKangaskhanScript:
+OlivineRestaurantJigglypuffScript:
+	jumptextfaceplayer OlivineRestaurantPokemonText
+
 OlivineRestaurantLassText:
 	text "Welcome!"
+	done
+
+OlivineRestaurantPokemonText:
+	text " !"
 	done
 
 OlivineRestaurant_MapEvents:
@@ -26,3 +34,22 @@ OlivineRestaurant_MapEvents:
 
 	def_object_events
 	object_event  5,  8, SPRITE_LASS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLACK, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantLassScript, -1
+	object_event  2,  4, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantKangaskhanScript, -1
+	object_event  2,  3, SPRITE_EKANS, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
+	object_event  8,  4, SPRITE_BEAUTY, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
+	object_event  5,  3, SPRITE_BAYLEEF, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
+	object_event 11,  3, SPRITE_SPEAROW, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
+	object_event 11,  4, SPRITE_RHYDON, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
+	object_event 11,  7, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
+	object_event 11,  8, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
+	object_event  8,  7, SPRITE_ROCKER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
+
+
+
+; ampharos, arcanine, bayleef, caterpie, crocanaw, cyndaquil, delibird
+; donphan, dunsparce, eevee, ekans, flaafy, furret, girafarig, gyarados
+; horsea, houndoom, houndour, kingdra, lanturn, lapras, larvitar, magcargo
+; magikarp, mantine, mareep, meganium, murkrow, natu, omanyte, omastar, persian
+; phanpy, ponyta, porygon, porygon2, quagsire, rapidash, remoraid, rhydon
+; scizor, seadra, seaking, shuckle, skarmory, slugma, smeargle, spearow
+; totodile, tyranitar, xatu
