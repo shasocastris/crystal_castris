6a6f3abe5aa7a9ca14deadcf91d6b47df5770652
shasocastris <fleetadmiralcaine@gmail.com>
2026-02-07 22:02:23 -0700

    ShowPokedexEntry for pokemon in restaurant

--- maps/OlivineRestaurant.asm
+++ maps/OlivineRestaurant.asm
@@ -9,16 +9,133 @@ OlivineRestaurant_MapScripts:
 OlivineRestaurantLassScript:
 	jumptextfaceplayer OlivineRestaurantLassText
 
-OlivineRestaurantKangaskhanScript:
-OlivineRestaurantJigglypuffScript:
-	jumptextfaceplayer OlivineRestaurantPokemonText
+OlivineRestaurantYoungsterScript:
+	jumptext OlivineRestaurantYoungsterText
+
+OlivineRestaurantBeautyScript:
+	jumptextfaceplayer OlivineRestaurantBeautyText
+
+OlivineRestaurantTeacherScript:
+	jumptextfaceplayer OlivineRestaurantTeacherText
+
+OlivineRestaurantRockerScript:
+	jumptextfaceplayer OlivineRestaurantRockerText
+
+
+OlivineRestaurantEkansScript:
+	opentext
+	writetext OlivineRestaurantEkansText
+	cry EKANS
+	waitbutton
+	callasm .Ekans
+	special ShowPokedexEntry
+	closetext
+	end
+
+.Ekans
+	ld hl, EKANS
+	call GetPokemonIDFromIndex
+	ld [wScriptVar], a
+	ret
+
+OlivineRestaurantBayleefScript:
+	opentext
+	writetext OlivineRestaurantBayleefText
+	cry BAYLEEF
+	waitbutton
+	callasm .Bayleef
+	special ShowPokedexEntry
+	closetext
+	end
+
+.Bayleef
+	ld hl, BAYLEEF
+	call GetPokemonIDFromIndex
+	ld [wScriptVar], a
+	ret
+
+OlivineRestaurantSpearowScript:
+	opentext
+	writetext OlivineRestaurantSpearowText
+	cry SPEAROW
+	waitbutton
+	callasm .Spearow
+	special ShowPokedexEntry
+	closetext
+	end
+
+.Spearow
+	ld hl, SPEAROW
+	call GetPokemonIDFromIndex
+	ld [wScriptVar], a
+	ret
+
+OlivineRestaurantRhydonScript:
+	opentext
+	writetext OlivineRestaurantRhydonText
+	cry RHYDON
+	waitbutton
+	callasm .Rhydon
+	special ShowPokedexEntry
+	closetext
+	end
+
+.Rhydon
+	ld hl, RHYDON
+	call GetPokemonIDFromIndex
+	ld [wScriptVar], a
+	ret
 
 OlivineRestaurantLassText:
 	text "Welcome!"
 	done
 
-OlivineRestaurantPokemonText:
-	text " !"
+OlivineRestaurantYoungsterText:
+	text "Hey, EKANS!"
+
+	para "Quit trying to"
+	line "slither away!"
+	done
+
+OlivineRestaurantBeautyText:
+	text "I'm training my"
+	line "SPEAROW until it"
+	cont "evolves."
+
+	para "Then I'll have a"
+	line "FEAROW to fly me"
+	cont "anywhere!"
+	done
+
+OlivineRestaurantTeacherText:
+	text "The food here is"
+	line "excellent."
+
+	para "And they often let"
+	line "customers take"
+	cont "LEFTOVERS."
+	done
+
+OlivineRestaurantRockerText:
+	text "I think seeing all"
+	line "these #MON is"
+	cont "so exciting."
+	done
+
+OlivineRestaurantEkansText:
+	text "EKANS: Ek Ekans!"
+	done
+
+OlivineRestaurantBayleefText:
+	text "BAYLEEF: Li liif!"
+	done
+
+OlivineRestaurantSpearowText:
+	text "SPEAROW: Spear!"
+	done
+
+OlivineRestaurantRhydonText:
+	text "RHYDON: Rhy! Rhy!"
 	done
 
 OlivineRestaurant_MapEvents:
@@ -34,22 +151,11 @@ OlivineRestaurant_MapEvents:
 
 	def_object_events
 	object_event  5,  8, SPRITE_LASS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLACK, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantLassScript, -1
-	object_event  2,  4, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantKangaskhanScript, -1
-	object_event  2,  3, SPRITE_EKANS, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
-	object_event  8,  4, SPRITE_BEAUTY, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
-	object_event  5,  3, SPRITE_BAYLEEF, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
-	object_event 11,  3, SPRITE_SPEAROW, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
-	object_event 11,  4, SPRITE_RHYDON, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
-	object_event 11,  7, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
-	object_event 11,  8, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
-	object_event  8,  7, SPRITE_ROCKER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantJigglypuffScript, -1
-
-
-
-; ampharos, arcanine, bayleef, caterpie, crocanaw, cyndaquil, delibird
-; donphan, dunsparce, eevee, ekans, flaafy, furret, girafarig, gyarados
-; horsea, houndoom, houndour, kingdra, lanturn, lapras, larvitar, magcargo
-; magikarp, mantine, mareep, meganium, murkrow, natu, omanyte, omastar, persian
-; phanpy, ponyta, porygon, porygon2, quagsire, rapidash, remoraid, rhydon
-; scizor, seadra, seaking, shuckle, skarmory, slugma, smeargle, spearow
-; totodile, tyranitar, xatu
+	object_event  2,  4, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantYoungsterScript, -1
+	object_event  2,  3, SPRITE_EKANS, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantEkansScript, -1
+	object_event  5,  3, SPRITE_BAYLEEF, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantBayleefScript, -1
+	object_event  8,  4, SPRITE_BEAUTY, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantBeautyScript, -1
+	object_event 11,  3, SPRITE_SPEAROW, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantSpearowScript, -1
+	object_event 11,  4, SPRITE_RHYDON, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantRhydonScript, -1
+	object_event 11,  7, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantTeacherScript, -1
+	object_event  8,  7, SPRITE_ROCKER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, OlivineRestaurantRockerScript, -1
