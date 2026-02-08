6a721b2874292b912cbcea84160d86c6f2aeea57
shasocastris <fleetadmiralcaine@gmail.com>
2026-02-07 20:15:40 -0700

    warp updates

--- maps/OlivineRestaurant.asm
+++ maps/OlivineRestaurant.asm
@@ -17,8 +17,8 @@ OlivineRestaurant_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  2,  9, OLIVINE_CITY, 8
-	warp_event  3,  9, OLIVINE_CITY, 8
+	warp_event  2,  9, OLIVINE_CITY, 7
+	warp_event  3,  9, OLIVINE_CITY, 7
 
 	def_coord_events
 
