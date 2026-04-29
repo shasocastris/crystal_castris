2f139b7bd2ccd10fa05b5f29cab949bd7379e9f0
shasocastris <fleetadmiralcaine@gmail.com>
2026-04-29 13:20:55 -0600

    Add items to MountMoonPath.asm

--- maps/MountMoonPath.asm
+++ maps/MountMoonPath.asm
@@ -1,9 +1,22 @@
 	object_const_def
+	const MOUNTMOONPATH_POKE_BALL1
+	const MOUNTMOONPATH_POKE_BALL2
 
 MountMoonPath_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+MountMoonPathMaxPotion:
+	itemball MAX_POTION
+
+MountMoonPathFullHeal:
+	itemball FULL_HEAL
+
+MountMoonPathHiddenMaxRepel:
+	hiddenitem MAX_REPEL, EVENT_MT_MOON_PATH_HIDDEN_MAX_REPEL
+
+MountMoonPathHiddenStardust:
+	hiddenitem STARDUST, EVENT_MT_MOON_PATH_HIDDEN_STARDUST
 
 MountMoonPath_MapEvents:
 	db 0, 0 ; filler
@@ -15,5 +28,9 @@ MountMoonPath_MapEvents:
 	def_coord_events
 
 	def_bg_events
+	bg_event 10,  5, BGEVENT_ITEM, MountMoonPathHiddenMaxRepel
+	bg_event  6, 12, BGEVENT_ITEM, MountMoonPathHiddenStardust
 
 	def_object_events
+	object_event 13,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MountMoonPathMaxPotion, EVENT_MT_MOON_PATH_MAX_POTION
+	object_event 14, 10, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MountMoonPathFullHeal,   EVENT_MT_MOON_PATH_FULL_HEAL
