ef81b5c3fcc6055f0fb9942dbc87a931ec7acdff
shasocastris <fleetadmiralcaine@gmail.com>
2026-04-29 23:55:54 -0600

    Update final fight script

--- maps/CeruleanBase3.asm
+++ maps/CeruleanBase3.asm
@@ -2,6 +2,8 @@
 	const CERULEANBASE3_ARIANA
 	const CERULEANBASE3_ARCHER
 	const CERULEANBASE3_MEWTWO
+	const CERULEANBASE3_LANCE
+	const CERULEANBASE3_FIRST_RANGER
 
 CeruleanBase3_MapScripts:
 	def_scene_scripts
@@ -28,24 +30,33 @@ TrainerExecutiveFAriana:
 	applymovement PLAYER, CeruleanBase3PlayerApproachArcherMovement
 	end
 
+CeruleanBase3PlayerApproachArcherMovement:
+	step RIGHT
+	step UP
+	step UP
+	step UP
+	step UP
+	step UP
+	step UP
+	step UP
+	step_end
+
 ArianaBase3BeforeText:
 	text "…You again."
 
-	para "RUINS OF ALPH."
-	line "I remember you."
+	para "Don't think I've"
+	line "forgotten how you"
+	cont "ruined our plans"
+	cont "at the RUINS OF"
+	cont "ALPH."
 
 	para "You cost us months"
 	line "of work in JOHTO."
 
-	para "I had hoped you"
-	line "wouldn't make it"
-	cont "this far."
-
-	para "ARCHER is just"
-	line "past me."
-
-	para "You will not reach"
-	line "him."
+	para "It is high time"
+	line "you paid for"
+	cont "interfering with"
+	cont "our operations."
 	done
 
 ArianaBase3WinText:
@@ -55,32 +66,22 @@ ArianaBase3WinText:
 ArianaBase3AfterText:
 	text "…"
 
-	para "Three times now."
-
 	para "You have beaten"
 	line "me three times."
 
 	para "I won't make"
 	line "excuses."
 
-	para "Go."
-	line "Face ARCHER."
+	para "Perhaps it is for"
+	line "the best."
 
-	para "But know that what"
-	line "he's built in"
-	cont "there cannot be"
-	cont "dismantled by"
-	cont "winning a battle."
-
-	para "The #MON in"
-	line "that chamber is"
-	cont "beyond any of us"
-	cont "now."
+	para "I can watch your"
+	line "entire team get"
+	cont "crushed by a"
+	cont "single #MON."
 	done
 
 CeruleanBase3ArcherApproachScript:
-	checkevent EVENT_BEAT_ROCKET_EXECUTIVEF_3
-	iffalse .NotReady
 	turnobject CERULEANBASE3_ARCHER, DOWN
 	showemote EMOTE_SHOCK, CERULEANBASE3_ARCHER, 15
 	special FadeOutMusic
@@ -102,27 +103,68 @@ CeruleanBase3ArcherApproachScript:
 	writetext ArcherBase3AfterText
 	waitbutton
 	closetext
-	playsound SFX_STRENGTH
-	earthquake 60
-	refreshmap
 	; Mewtwo breaks free
+	playsound SFX_STRENGTH
+	earthquake 120
 	cry MEWTWO
-	showemote EMOTE_SHOCK, CERULEANBASE3_MEWTWO, 20
+	applymovement CERULEANBASE3_MEWTWO, CeruleanBase3MewtwoFleeMovement1
+	turnobject CERULEANBASE3_MEWTWO, RIGHT
+	showemote EMOTE_SHOCK, CERULEANBASE3_MEWTWO, 30
 	pause 30
+	playsound SFX_WARP_TO
+	special FadeOutToWhite
+	pause 30
+	special LoadMapPalettes
+	special FadeInPalettes_EnableDynNoApply
 	disappear CERULEANBASE3_MEWTWO
-	; Archer retreats
+	waitsfx
+	refreshmap
+	pause 30
+	opentext
+	writetext ArcherCursesText
+	waitbutton
+	writetext LanceIThinkNotText
+	waitbutton
+	closetext
+	; Lance and First Ranger appear
+	appear CERULEANBASE3_LANCE
+	appear CERULEANBASE3_FIRST_RANGER
+	follow CERULEANBASE3_LANCE, CERULEANBASE3_FIRST_RANGER
+	applymovement CERULEANBASE3_LANCE, CeruleanBase3LanceAppearMovement
+	turnobject CERULEANBASE3_LANCE, RIGHT
+	turnobject CERULEANBASE3_FIRST_RANGER, RIGHT
+	turnobject PLAYER, LEFT
+	turnobject CERULEANBASE3_ARCHER, LEFT
+	opentext
+	writetext LanceCapturesArcherText
+	waitbutton
+	writetext FirstRangerThanksText
+	waitbutton
+	closetext
+	follow CERULEANBASE3_LANCE, CERULEANBASE3_ARCHER
+	applymovement CERULEANBASE3_LANCE, CeruleanBase3LanceArrestsArcherMovement
+	pause 10
+	applymovement CERULEANBASE3_FIRST_RANGER, CeruleanBase3FirstRangerLeavesMovement
+	disappear CERULEANBASE3_LANCE
 	disappear CERULEANBASE3_ARCHER
+	disappear CERULEANBASE3_FIRST_RANGER
+	disappear CERULEANBASE3_ARIANA
 	setscene SCENE_CERULEANBASE3_NOOP
 	setevent EVENT_CLEARED_CERULEAN_CAVE
 	playmapmusic
 	end
 
-.NotReady:
-	end
+CeruleanBase3MewtwoFleeMovement1:
+	step DOWN
+	step_end
 
-CeruleanBase3PlayerApproachArcherMovement:
-	step RIGHT
-	step UP
+CeruleanBase3MewtwoFleeMovement2:
+	jump_step DOWN
+	jump_step DOWN
+	jump_step DOWN
+	step_end
+
+CeruleanBase3LanceAppearMovement:
 	step UP
 	step UP
 	step UP
@@ -131,62 +173,135 @@ CeruleanBase3PlayerApproachArcherMovement:
 	step UP
 	step_end
 
+CeruleanBase3LanceArrestsArcherMovement:
+	step LEFT
+	step DOWN
+	step DOWN
+	step DOWN
+	step DOWN
+	step DOWN
+	step DOWN
+	step DOWN
+	step DOWN
+	step_end
+
+CeruleanBase3FirstRangerLeavesMovement:
+	step DOWN
+	step DOWN
+	step DOWN
+	step DOWN
+	step DOWN
+	step DOWN
+	step_end
+
 ArcherBase3BeforeText:
-	text "So."
-	line "You reached me."
+	text "ARIANA has kept me"
+	line "well informed"
+	cont "about the brat who"
+	cont "helped KURT rescue"
+	cont "the SLOWPOKE in"
+	cont "AZALEA TOWN."
 
-	para "I'll be direct."
-	line "GIOVANNI spent"
-	cont "years trying to"
-	cont "capture MEWTWO."
-	cont "He failed."
+	para "That was small fry"
+	line "which was used to"
+	cont "finish funding"
+	cont "this operation."
 
-	para "I used a MASTER"
-	line "BALL he left"
-	cont "behind."
+	para "And now it's"
+	line "complete!"
 
-	para "Six weeks in this"
-	line "base. The right"
-	cont "frequency. The"
-	cont "right equipment."
+	para "When MEWTWO fled"
+	line "to this cave to"
+	cont "hide, we hunted it"
+	cont "down and caught it"
+	cont "with a single"
+	cont "MASTER BALL."
 
-	para "And now it obeys."
+	para "That's something"
+	line "not GIOVANNI was"
+	cont "able to do!"
 
-	para "This is the future"
-	line "of TEAM ROCKET."
-	cont "Power beyond what"
-	cont "GIOVANNI imagined."
+	para "And now you get to"
+	line "face the ultimate"
+	cont "#MON, and a"
+	cont "thorn in our side"
+	cont "will be removed"
+	cont "permanently."
 
-	para "You will not take"
-	line "it from me."
+	para "For the glory of"
+	line "TEAM ROCKET!"
 	done
 
 ArcherBase3WinText:
-	text "The signal…"
-	line "breaking down…"
+	text "You are such a"
+	line "wretched child…"
 	done
 
 ArcherBase3AfterText:
-	text "Without me to"
-	line "maintain the"
-	cont "frequency…"
-
-	para "The conditioning"
-	line "won't hold."
-
-	para "Six weeks of work."
-
-	para "MEWTWO was never"
-	line "truly ours."
-	cont "We only borrowed"
-	cont "its obedience."
+	text "Even the armor"
+	line "developed to focus"
+	cont "MEWTWO's psychic"
+	cont "energy was not"
+	cont "enough?"
 	done
 
-ArcherBase3BlockedText:
-	text "Defeat ARIANA"
-	line "first."
+ArcherCursesText:
+	text "And of course, now"
+	line "the worthless"
+	cont "#MON has fled"
+	cont "again."
+
+	para "The years, the"
+	line "effort, all"
+	cont "wasted!"
+
+	para "No matter. TEAM"
+	line "ROCKET survived"
+	cont "when GIOVANNI"
+	cont "vanished."
+
+	para "We'll recover from"
+	line "this as well."
 	done
 
+LanceIThinkNotText:
+	text "LANCE:"
+
+	para "I think not."
+	done
+
+LanceCapturesArcherText:
+	text "ROCKET EXECUTIVE"
+	line "ARCHER…"
+
+	para "We've finally"
+	line "caught up to you."
+
+	para "First a WILD HUNT"
+	line "and then trying"
+	cont "to unleash MEWTWO?"
+
+	para "You and ARIANA"
+	line "will never see"
+	cont "daylight again."
+	done
+
+FirstRangerThanksText:
+	text "FIRST RANGER:"
+
+	para "You have our "
+	line "deepest thanks,"
+	cont "<PLAY_G>."
+
+	para "<RIVAL>, too. He"
+	line "was the one who"
+	cont "lead us here after"
+	cont "you found the"
+	cont "entrance."
+
+	para "He's waiting for"
+	line "you upstairs."
+	done
 
 CeruleanBase3_MapEvents:
 	db 0, 0 ; filler
@@ -202,3 +317,5 @@ CeruleanBase3_MapEvents:
 	object_event  5,  9, SPRITE_ROCKET_GIRL, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_TRAINER, 1, TrainerExecutiveFAriana, EVENT_CLEARED_CERULEAN_CAVE
 	object_event  6,  2, SPRITE_ROCKET, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_CLEARED_CERULEAN_CAVE
 	object_event  5,  2, SPRITE_MONSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_CLEARED_CERULEAN_CAVE
+	object_event  5,  8, SPRITE_LANCE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_DISCOVERED_CERULEAN_CAVE
+	object_event  6,  8, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLACK, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_DISCOVERED_CERULEAN_CAVE
