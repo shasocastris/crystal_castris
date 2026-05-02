85d0aa3aa8a387f5ba98fbea2a0f75f8c603c8ec
shasocastris <fleetadmiralcaine@gmail.com>
2026-05-01 23:27:41 -0600

    Swap amulet coin and nugget locations for routing

--- maps/GoldenrodDeptStoreB1F.asm
+++ maps/GoldenrodDeptStoreB1F.asm
@@ -41,8 +41,8 @@ GoldenrodDeptStoreB1FMachopScript:
 GoldenrodDeptStoreB1FEther:
 	itemball ETHER
 
-GoldenrodDeptStoreB1FAmuletCoin:
-	itemball AMULET_COIN
+GoldenrodDeptStoreB1FNugget:
+	itemball NUGGET
 
 GoldenrodDeptStoreB1FBurnHeal:
 	itemball BURN_HEAL
@@ -97,7 +97,7 @@ GoldenrodDeptStoreB1F_MapEvents:
 
 	def_object_events
 	object_event 10, 15, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, GoldenrodDeptStoreB1FEther, EVENT_GOLDENROD_DEPT_STORE_B1F_ETHER
-	object_event 14,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, GoldenrodDeptStoreB1FAmuletCoin, EVENT_GOLDENROD_DEPT_STORE_B1F_AMULET_COIN
+	object_event 14,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, GoldenrodDeptStoreB1FNugget, EVENT_GOLDENROD_DEPT_STORE_B1F_NUGGET
 	object_event  6,  3, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, GoldenrodDeptStoreB1FBurnHeal, EVENT_GOLDENROD_DEPT_STORE_B1F_BURN_HEAL
 	object_event 15, 15, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, GoldenrodDeptStoreB1FUltraBall, EVENT_GOLDENROD_DEPT_STORE_B1F_ULTRA_BALL
 	object_event  9, 10, SPRITE_BLACK_BELT, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, GoldenrodDeptStoreB1FBlackBelt1Script, -1
