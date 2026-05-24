4fc051404acd3cd1a5dd8fe5396b426ec8afdfec
shasocastris <fleetadmiralcaine@gmail.com>
2026-05-24 12:01:25 -0600

    Play encounter music for phone contact rematches

--- engine/events/std_scripts.asm
+++ engine/events/std_scripts.asm
@@ -1164,6 +1164,8 @@ PhoneFullMScript:
 	end
 
 RematchMScript:
+	loadtemptrainer
+	encountermusic
 	readvar VAR_CALLERID
 	ifequal PHONE_SCHOOLBOY_JACK, .Jack
 	ifequal PHONE_SAILOR_HUEY, .Huey
@@ -1639,6 +1641,8 @@ PhoneFullFScript:
 	end
 
 RematchFScript:
+	loadtemptrainer
+	encountermusic
 	readvar VAR_CALLERID
 	ifequal PHONE_COOLTRAINERF_BETH, .Beth
 	ifequal PHONE_COOLTRAINERF_REENA, .Reena
