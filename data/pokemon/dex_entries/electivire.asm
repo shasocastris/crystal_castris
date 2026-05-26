98c214b8468896661cf132795f60518a93e14f7d
shasocastris <fleetadmiralcaine@gmail.com>
2026-05-25 20:43:26 -0600

    Add files that were not properly added to the repo

---
+++ data/pokemon/dex_entries/electivire.asm
@@ -0,0 +1,10 @@
+	db "THNDRBOLT@" ; species name
+	dw 180, 1385 ; height, weight
+
+	db   "ELECTIVIRE grabs"
+	next "foes with its twin"
+	next "tails to deliver"
+
+	page "jolts of over"
+	next "20,000 volts of"
+	next "electricity.@"
