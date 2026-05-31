a622cfeaf65b171443c832c248fa89100bd4b350
shasocastris <fleetadmiralcaine@gmail.com>
2026-05-31 17:22:48 -0600

    Revert "Update existing pokemon to have stats and evs of final forms of later generations"
    This reverts commit 8bc245a4353df5ec406e0c69617627c83d869576.

--- data/pokemon/base_stats/togetic.asm
+++ data/pokemon/base_stats/togetic.asm
@@ -1,7 +1,7 @@
 	db 0 ; species ID placeholder
 
-	db  85,  50,  95,  80, 120, 115
-	evs  0,   0,   0,   0,   2,   1
+	db  55,  40,  85,  40,  80, 105
+	evs  0,   0,   0,   0,   0,   2
 	;   hp  atk  def  spd  sat  sdf
 
 	db FAIRY, FLYING ; type
