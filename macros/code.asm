3239e61e4355881b4038dd5511f4634b7d31aade
shasocastris <fleetadmiralcaine@gmail.com>
2026-07-15 10:20:02 -0600

    Introduce changebridgeblock macro from PolishedCrystal

--- macros/code.asm
+++ macros/code.asm
@@ -67,3 +67,11 @@ MACRO cphl16
 	ENDC
 .done\@
 ENDM
+
+MACRO changebridgeblock
+	; lb de, \1 + 4, \2 + 4
+	; call GetBlockLocation
+	ld hl, wOverworldMapBlocks + (\2 / 2 + 3) * (\4_WIDTH + 6) + \1 / 2 + 3
+	; hard-coding the above calculation for efficiency
+	ld [hl], \3
+ENDM
