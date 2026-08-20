485df99a4c961481700732f8f271b220b890dfaf
shasocastris <fleetadmiralcaine@gmail.com>
2026-08-20 16:32:32 -0600

    fix fury strikes!

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -46,7 +46,7 @@ MovesGen1:
 	move EFFECT_FLINCH_HIT,         60, FIGHTING,      PHYSICAL,  85, 15,  30      ;ROLLING_KICK
 	move EFFECT_FLINCH_HIT,         70, NORMAL,        PHYSICAL, 100, 15,  30      ;HEADBUTT
 	move EFFECT_NORMAL_HIT,         65, NORMAL,        PHYSICAL, 100, 25,   0      ;HORN_ATTACK
-	move FURY_STRIKES, EFFECT_MULTI_HIT,          15, NORMAL,        85, 20,   0
+	move EFFECT_MULTI_HIT,          15, NORMAL,        PHYSICAL,  85, 20,   0      ;FURY_STRIKES
 	move EFFECT_PARALYZE_HIT,       85, NORMAL,        PHYSICAL, 100, 15,  30      ;BODY_SLAM
 	move EFFECT_TRAP_TARGET,        15, NORMAL,        PHYSICAL, 100, 20,   0      ;WRAP
 	move EFFECT_RECOIL_HIT,         90, NORMAL,        PHYSICAL,  85, 20,   0      ;TAKE_DOWN
